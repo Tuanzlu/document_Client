08528ffcb6eea2c06584bad1513d16d2db80ecf2
Tuanz_lu <tuanz_lu@Tuanz-ludeMacBook-Pro.local>
2020-08-10 14:40:50 +0800

    新增入口首页

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,5 +1,15 @@
 <template>
-  <div></div>
+  <div>
+    <div class="content">
+      <div class="title">
+        金刚石文档
+      </div>
+      <p class="intro">
+        在线编辑
+      </p>
+      <a-button type="default" class="startBtn" @click="Login">开始使用</a-button>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -7,7 +17,37 @@ export default {
   data() {
     return {};
   },
-  methods: {}
+  methods: {
+    Login() {
+      this.$router.push({
+        name: "Login",
+      });
+    },
+  },
 };
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.content {
+  // border: 2px red solid;
+  width: 100%;
+  height: 1000px;
+  background: url("../assets/bg.jpeg") no-repeat center bottom;
+}
+.title {
+  // border: 2px red solid;
+  font-size: 40px;
+  color: white;
+  text-align: center;
+  padding-top: 200px;
+}
+.intro {
+  line-height: 50px;
+  text-align: center;
+  font-size: 24px;
+  color: white;
+}
+.startBtn {
+  display: block;
+  margin: 0 auto;
+}
+</style>
