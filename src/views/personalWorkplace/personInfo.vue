22351f98ba6b0d6e1a6d5c591e846e881dcbcb3c
ruiming0114 <ruiming0114@buaa.edu.cn>
2020-08-19 21:50:38 +0800

    个人信息页面背景修改

--- src/views/personalWorkplace/personInfo.vue
+++ src/views/personalWorkplace/personInfo.vue
@@ -1,143 +1,152 @@
 <template>
-<div class="total">
-  <div class="Info_container">
-     <a-button class="btn" @click="toLast">
-          <a-icon type="left" />
-     </a-button>
+  <div class="total">
+    <div class="Info_container">
+      <a-button class="btn" @click="toLast">
+        <a-icon type="left" />
+      </a-button>
       <p>个 人 信 息</p>
       <div class="Info_box">
         <div style="float:left;">
-        <uploadPhoto></uploadPhoto>
+          <uploadPhoto></uploadPhoto>
         </div>
-        </br>
+        <br />
         <!--分割线-->
-        <div :class="{loginusermode:judgement}"  style="float:left;width: 2px;height: 300px; background: #000;margin-right: 40px;margin-left: 20px;"></div>  
+        <div
+          :class="{loginusermode:judgement}"
+          style="float:left;width: 2px;height: 300px; background: #000;margin-right: 40px;margin-left: 20px;"
+        ></div>
         <!--右边内容-->
         <a-list size="large">
-            <div>
-                <a-icon type="user" style="margin-right: 10px"/> 
-                <span> 用户名：{{info.username}}</span>
-            </div>
-            </br></br>
-            <div>
-                <a-icon type="contacts"  style="margin-right: 10px ;float: center"/>
-                <span> 账号ID：{{info.userid}}</span>
-            </div>
-            <br v-if="judgement" /> <br  v-if="judgement" />
-            <div v-if="judgement" >
-                <a-icon type="lock"  style="margin-right: 10px"/>
-                <span> 密码：{{info.password}}</span>
-                <a-button v-show="judgement" @click="toPwd" type="link" style="float: right;">修改</a-button>
-                <inputPwd ref="choosePwd" v-show="showPwd" v-on:closePwd="closePwd"></inputPwd>
-            </div>
-             </br></br>
-            <div>
-                <a-icon type="wechat"  style="margin-right: 10px"/>
-                <span> 微信：{{info.wechat}}</span>
-                <a-button v-show="judgement" @click="toWechat" type="link" style="float: right;">修改</a-button>
-                <inputBox ref="chooseButton" v-show="showWechat" v-on:closeme="closeme"></inputBox>
-            </div>
-            </br></br>
-            <div>
-                <a-icon type="mail"  style="margin-right: 10px"/>
-                <span> 邮箱：{{info.email}}</span>
-                <a-button v-show="judgement" @click="toEmail" type="link" style="float: right;">修改</a-button>
-                <inputEmail ref="chooseE" v-show="showEmail" v-on:closeEmail="closeEmail"></inputEmail>
-            </div>
-            </br></br>
-            <div>
-                <a-icon type="smile"  style="margin-right: 10px"/>
-                <span> 个性签名：{{info.intro}}</span>
-                <a-button v-show="judgement" @click="toIntro" type="link" style="float: right;">修改</a-button>
-                <inputIntro ref="chooseIntro" v-show="showIntro" v-on:closeEmail="closeIntro"></inputIntro>
-            </div>
-            <br v-if="judgement" /> <br  v-if="judgement" />
-            <div v-if="judgement" >
-               <a-icon type="question-circle" style="margin-right: 10px"/>
-                <span> 密保问题：{{info.question}}</span></br></br>
-                <span style="margin-left: 28px" > 问题答案：{{info.answer}}</span>
-                <a-button v-show="judgement" @click="toQnA" type="link" style="float: right;">修改</a-button>
-                <inputQnA ref="chooseQnA" v-show="showQnA" v-on:closeEmail="closeQnA"></inputQnA>
-            </div>
+          <div>
+            <a-icon type="user" style="margin-right: 10px" />
+            <span>用户名：{{info.username}}</span>
+          </div>
+          <br />
+          <br />
+          <div>
+            <a-icon type="contacts" style="margin-right: 10px ;float: center" />
+            <span>账号ID：{{info.userid}}</span>
+          </div>
+          <br v-if="judgement" />
+          <br v-if="judgement" />
+          <div v-if="judgement">
+            <a-icon type="lock" style="margin-right: 10px" />
+            <span>密码：{{info.password}}</span>
+            <a-button v-show="judgement" @click="toPwd" type="link" style="float: right;">修改</a-button>
+            <inputPwd ref="choosePwd" v-show="showPwd" v-on:closePwd="closePwd"></inputPwd>
+          </div>
+          <br />
+          <br />
+          <div>
+            <a-icon type="wechat" style="margin-right: 10px" />
+            <span>微信：{{info.wechat}}</span>
+            <a-button v-show="judgement" @click="toWechat" type="link" style="float: right;">修改</a-button>
+            <inputBox ref="chooseButton" v-show="showWechat" v-on:closeme="closeme"></inputBox>
+          </div>
+          <br />
+          <br />
+          <div>
+            <a-icon type="mail" style="margin-right: 10px" />
+            <span>邮箱：{{info.email}}</span>
+            <a-button v-show="judgement" @click="toEmail" type="link" style="float: right;">修改</a-button>
+            <inputEmail ref="chooseE" v-show="showEmail" v-on:closeEmail="closeEmail"></inputEmail>
+          </div>
+          <br />
+          <br />
+          <div>
+            <a-icon type="smile" style="margin-right: 10px" />
+            <span>个性签名：{{info.intro}}</span>
+            <a-button v-show="judgement" @click="toIntro" type="link" style="float: right;">修改</a-button>
+            <inputIntro ref="chooseIntro" v-show="showIntro" v-on:closeEmail="closeIntro"></inputIntro>
+          </div>
+          <br v-if="judgement" />
+          <br v-if="judgement" />
+          <div v-if="judgement">
+            <a-icon type="question-circle" style="margin-right: 10px" />
+            <span>密保问题：{{info.question}}</span>
+            <br />
+            <br />
+            <span style="margin-left: 28px">问题答案：{{info.answer}}</span>
+            <a-button v-show="judgement" @click="toQnA" type="link" style="float: right;">修改</a-button>
+            <inputQnA ref="chooseQnA" v-show="showQnA" v-on:closeEmail="closeQnA"></inputQnA>
+          </div>
         </a-list>
-      </div> 
+      </div>
+    </div>
   </div>
-</div>
 </template>
 
 <script>
 import { getData } from "@/api/webget";
 import { putData } from "@/api/webput";
-import uploadPhoto from '@/components/uploadPhoto';
-import inputBox from '@/components/inputBox';
-import inputEmail from '@/components/inputEmail';
-import inputPwd from '@/components/inputPwd';
-import inputIntro from '@/components/inputIntro';
-import inputQnA from '@/components/inputQnA';
+import uploadPhoto from "@/components/uploadPhoto";
+import inputBox from "@/components/inputBox";
+import inputEmail from "@/components/inputEmail";
+import inputPwd from "@/components/inputPwd";
+import inputIntro from "@/components/inputIntro";
+import inputQnA from "@/components/inputQnA";
 export default {
   components: {
-        uploadPhoto,
-        inputBox,
-        inputEmail,
-        inputPwd,
-        inputIntro,
-        inputQnA
+    uploadPhoto,
+    inputBox,
+    inputEmail,
+    inputPwd,
+    inputIntro,
+    inputQnA
   },
   data() {
     return {
       info: [],
-      showWechat:false,
+      showWechat: false,
       showEmail: false,
       showPwd: false,
       showIntro: false,
-      showQnA:false,
-      judgement:false,
+      showQnA: false,
+      judgement: false
     };
   },
   methods: {
-    toLast(){
+    toLast() {
       console.log(this.$route.path);
-      let userid=(parseInt(window.sessionStorage.getItem("UserId")) );
-      if(this.$route.query.userid== userid){
-         this.$router.push("/used");
-      }else if(this.$route.query.userid!= userid){
+      let userid = parseInt(window.sessionStorage.getItem("UserId"));
+      if (this.$route.query.userid == userid) {
+        this.$router.push("/used");
+      } else if (this.$route.query.userid != userid) {
         this.$router.go(-1);
         this.$router.go(-1);
       }
-      
     },
     email_blur(e) {
       var verify = /^\w[-\w.+]*@([A-Za-z0-9][-A-Za-z0-9]+\.)+[A-Za-z]{2,14}/;
       if (!verify.test(e)) {
-          return false;
+        return false;
       } else {
-          return true;
+        return true;
       }
     },
-    toWechat(){
+    toWechat() {
       this.showWechat = !this.showWechat;
     },
-    toEmail(){
+    toEmail() {
       this.showEmail = !this.showEmail;
     },
-    toPwd(){
+    toPwd() {
       this.showPwd = !this.showPwd;
     },
-    toIntro(){
+    toIntro() {
       this.showIntro = !this.showIntro;
     },
-    toQnA(){
+    toQnA() {
       this.showQnA = !this.showQnA;
     },
-    closeQnA(){
+    closeQnA() {
       let res = this.$refs.chooseQnA.getChoose();
       this.showQnA = !this.showQnA;
       console.log(res.flag);
-      if(res.flag == 1 && (res.ques.length == 0 || res.ans.length ==0 )){
+      if (res.flag == 1 && (res.ques.length == 0 || res.ans.length == 0)) {
         this.$message.error("请输入修改后的结果");
         //console.log("正常关闭");
-      }
-      else if(res.flag == 1){
+      } else if (res.flag == 1) {
         this.info.question = res.ques;
         this.info.answer = res.ans;
         console.log(this.info.question);
@@ -145,59 +154,53 @@ export default {
         this.changeWE();
       }
     },
-    closeIntro(){
+    closeIntro() {
       let res = this.$refs.chooseIntro.getChoose();
       this.showIntro = !this.showIntro;
       console.log(res.flag);
-      if(res.flag == 1 && res.intro.length == 0){
+      if (res.flag == 1 && res.intro.length == 0) {
         this.$message.error("请输入修改后的结果");
         //console.log("正常关闭");
-      }
-      else if(res.flag == 1){
+      } else if (res.flag == 1) {
         this.info.intro = res.intro;
         console.log(this.info.intro);
         this.changeWE();
       }
     },
-    closePwd(){
+    closePwd() {
       let res = this.$refs.choosePwd.getChoose();
       this.showPwd = !this.showPwd;
       console.log(res);
-      if(res.flag === 0){
-        if(res.new !== res.confirm){
+      if (res.flag === 0) {
+        if (res.new !== res.confirm) {
           this.$message.error("请检查新密码和确认密码是否一致");
-        }
-        else if(res.confirm.length < 3){
+        } else if (res.confirm.length < 3) {
           this.$message.error("密码必须大于三位");
+        } else {
+          // console.log(res.confirm.length);
+          this.changePa(res.old, res.new);
         }
-        else{
-         // console.log(res.confirm.length);
-          this.changePa(res.old,res.new);
-        }
-      }
-      else{
-        if(res[3] === 1)
-          this.$message.error("请输入完全");
+      } else {
+        if (res[3] === 1) this.$message.error("请输入完全");
       }
     },
-    closeEmail(){
+    closeEmail() {
       let res = this.$refs.chooseE.getChoose();
       this.showEmail = !this.showEmail;
       let select = this.email_blur(res);
       console.log(res);
-      if(!select && res !== 'dis'){
+      if (!select && res !== "dis") {
         this.$message.error("请检查邮箱输入格式");
-      }
-      else if(res !== 'dis'){
+      } else if (res !== "dis") {
         this.info.email = res;
         console.log(this.info.email);
         this.changeWE();
       }
     },
-    closeme(){
+    closeme() {
       let res = this.$refs.chooseButton.getChoose();
       this.showWechat = !this.showWechat;
-      if(res){
+      if (res) {
         this.info.wechat = res;
         console.log(this.info.wechat);
         this.changeWE();
@@ -208,8 +211,8 @@ export default {
       let userId = this.$route.query.userid;
       params.append("userid", userId);
       let url = this.$urlPath.website.getUserInfo;
-      getData(url,params).then((res) => {
-        console.log( res.data);
+      getData(url, params).then(res => {
+        console.log(res.data);
         if (res.code === "0") {
           this.info = res.data;
           this.info.password = "●●●●●●●●●●●●";
@@ -221,9 +224,9 @@ export default {
         }
       });
     },
-    changeWE(){
+    changeWE() {
       let params = new URLSearchParams();
-      let userId = parseInt(window.sessionStorage.getItem('UserId'));
+      let userId = parseInt(window.sessionStorage.getItem("UserId"));
       params.append("userid", userId);
       params.append("wechat", this.info.wechat);
       params.append("email", this.info.email);
@@ -232,12 +235,12 @@ export default {
       params.append("question", this.info.question);
       params.append("answer", this.info.answer);
       let url = this.$urlPath.website.updateUserInfo;
-      putData(url,params).then((res) => {
+      putData(url, params).then(res => {
         console.log(res.code);
         if (res.code === "0") {
           this.$message.success("修改成功");
           this.info.wechat = res.data.wechat;
-          this.change = '';
+          this.change = "";
         } else if (res.code === "1") {
           this.$message.error("原密码输入错误");
         } else {
@@ -246,14 +249,14 @@ export default {
         }
       });
     },
-    changePa(oldpwd,newpwd){
+    changePa(oldpwd, newpwd) {
       let params = new URLSearchParams();
-      let userId = parseInt(window.sessionStorage.getItem('UserId'));
+      let userId = parseInt(window.sessionStorage.getItem("UserId"));
       params.append("userid", userId);
       params.append("oldpwd", oldpwd);
       params.append("newpwd", newpwd);
       let url = this.$urlPath.website.alterPassword;
-      putData(url,params).then((res) => {
+      putData(url, params).then(res => {
         console.log(res.code);
         if (res.code === "0") {
           this.$message.success("修改成功");
@@ -264,61 +267,58 @@ export default {
           this.$message.error("服务器返回时间间隔过长");
         }
       });
-    },
+    }
   },
   created() {
     this.getInfo();
   },
-  mounted(){
-    let loginuserid=(parseInt(window.sessionStorage.getItem("UserId")) );
+  mounted() {
+    let loginuserid = parseInt(window.sessionStorage.getItem("UserId"));
     console.log(loginuserid);
     console.log(this.$route.query.userid);
-    if(this.$route.query.userid==  loginuserid ){
-      this.judgement=true;
-    }else{
-      this.judgement=false;
+    if (this.$route.query.userid == loginuserid) {
+      this.judgement = true;
+    } else {
+      this.judgement = false;
     }
     console.log(this.judgement);
   }
-}
+};
 </script>
 
 <style>
 .total {
-  background:url("../../assets/bg.jpeg");
+  background: url("../../assets/bg.jpeg");
   width: 100%;
-  height: 800px;
-  background-size:cover
+  height: 1000px;
+  background-size: cover;
 }
 .Info_container {
-    width: 530px;
-    border-radius: 3px;
-    position: absolute;
-    left: 30%;
-    right: 30%;
-    top: 10%;
-    font-size: 30px;
-   
+  width: 530px;
+  border-radius: 3px;
+  position: absolute;
+  left: 30%;
+  right: 30%;
+  top: 10%;
+  font-size: 30px;
 }
-.Info_box{
- /* border:3px solid #000;*/
+.Info_box {
+  /* border:3px solid #000;*/
   height: 600px;
   width: 600px;
-  padding:50px 30px; 
+  padding: 50px 30px;
   margin-bottom: 50px;
   background-color: #fff;
-  opacity:0.9; 
+  opacity: 0.9;
 }
-.btn{
-  margin:10px 15px 0 -65px;
+.btn {
+  margin: 10px 15px 0 -65px;
   float: left;
 }
-.loginusermode{
-  height:470px!important;
+.loginusermode {
+  height: 470px !important;
 }
-.loginuserbox{
-  height:600px;
+.loginuserbox {
+  height: 600px;
 }
-
-
 </style>
